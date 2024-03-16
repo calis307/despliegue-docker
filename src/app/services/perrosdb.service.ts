d900e6e16d1e3e69ca86233a306d9f45654e4d66
calis307 <jclg10@educastillalamancha.es>
2024-03-16 17:40:59 +0100

    Se corrige ruta del environment y se modifica el compose a v2

--- src/app/services/perrosdb.service.ts
+++ src/app/services/perrosdb.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 
 @Injectable({
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment.prod';
 })
 export class PerrosService {
   // URL base para la API de perros
-  private baseUrl = environment.API_BASE_URL;
+  private baseUrl =  environment.API_BASE_URL;
 
   constructor(private http: HttpClient) {}
 
